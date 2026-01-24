af367606d04753553e08ffcfdf49679a8f99e036
Nicholas Doherty <nicholasdoherty95@gmail.com>
2026-01-24 14:17:37 -0500

    Downscale duplicate glyph to 90% and bump duplicate-to-end plugin to 1.10 in archetypes.

--- plugins/global-tool-use/duplicate-to-end.js
+++ plugins/global-tool-use/duplicate-to-end.js
@@ -3,7 +3,7 @@ const plugin = {
     id: 'duplicateToEnd',
     name: 'Duplicate to End',
     description: 'Adds button to duplicate a tool and move it to the end of the workflow',
-    _version: '1.9',
+    _version: '1.10',
     enabledByDefault: true,
     phase: 'mutation',
     initialState: { missingLogged: false },
@@ -88,7 +88,7 @@ const plugin = {
             
             dupToEndBtn.innerHTML = `
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="wf-duplicate-to-end-icon h-3.5 w-3.5 text-muted-foreground hover:text-primary">
-                    <g transform="translate(-2 3)">
+                    <g transform="translate(-2 3) scale(0.9)">
                         <line x1="15" x2="15" y1="12" y2="18"></line>
                         <line x1="12" x2="18" y1="15" y2="15"></line>
                         <rect width="14" height="14" x="8" y="8" rx="2" ry="2"></rect>
